b49faec5ea0485dfac7ce15baad8889f0ef9417e
Todd Pihl <todd.pihl@gmail.com>
2024-10-22 16:58:09 -0400

    Moved to configuration file

--- Bento-MDF2DataDictionary.py
+++ Bento-MDF2DataDictionary.py
@@ -4,6 +4,12 @@ import argparse
 import pandas as pd
 import requests
 from ruamel.yaml import YAML as RUAYAML
+import yaml
+
+def readYaml(yamlfile):
+    with open(yamlfile) as f:
+        configs = yaml.load(f, Loader=yaml.FullLoader)
+    return configs
 
 def runcaDSRQuery(cdeid, cdever):
     headers = {"accept" : "application/json"}
@@ -36,7 +42,16 @@ def writeFormattedYaml(filename, df):
         yamlfact.dump(yamldict, f)   
 
 def main(args):
-    mdf_working = MDF(args.modelfile, args.propfile, handle=args.handle)
+
+    configs = readYaml(args.config)
+
+    temp_files = []
+    for file in configs['Input']['mdffiles']:
+        temp_files.append(configs['Input']['in_dir']+file)
+    #mdf_files = ",".join(temp_files)
+    mdf_working = MDF(temp_files[0], temp_files[1], handle = configs['Input']['handle'])
+
+    #mdf_working = MDF(args.modelfile, args.propfile, handle=args.handle)s
 
     # First step is to create a dataframe of all properties that have allowable values, etiher as an enum section or as a CDE reference with PVs
     columns = ['Node', 'Property', 'Description', 'Required','Code','Origin','Version' ,'DataType', 'Enum']
@@ -67,17 +82,14 @@ def main(args):
         termhandle = None
         node = prop[0]
         propname = prop[1]
-        #print(props[prop].get_attr_dict())
         desc = props[prop].get_attr_dict()['desc']
         req = props[prop].get_attr_dict()['is_required']
         datatype = props[prop].get_attr_dict()['value_domain']
         prophandle = props[prop].get_attr_dict()['handle']
         if mdf_working.model.props[prop].concept is not None:
             workingterms = mdf_working.model.props[prop].concept.terms.values()
-            #print(workingterms)
             for workingtermobj in workingterms:
                 workingterm = workingtermobj.get_attr_dict()
-                #print(workingterm)
                 code = workingterm['origin_id']
                 if 'origin_version' in workingterm:
                     version = workingterm['origin_version']
@@ -93,36 +105,38 @@ def main(args):
 
         temp = {'Node' : node, 'Property': propname, 'Description':desc, 'Required':req,'Code':code,'Origin':origin,'Version':version ,'DataType':datatype, 'Enum':enum}
         final_df.loc[len(final_df.index)] = temp
-        #if termhandle is not None:
-        #    print(f"Prop Handle:\t{prophandle}\tTerm Handle:\t{termhandle}")
-        #print(temp)
-
-    outdir = '/home/pihl/mockdata/'    
+    #outdir = '/home/pihl/mockdata/'
+    outfile = configs['Output']['out_dir']+configs['Output']['filename']
 
     #Write a csv file
-    csvfile = outdir+args.output+".tsv"
+    csvfile = outfile+".tsv"
+    #csvfile = outdir+args.output+".tsv"
     final_df.to_csv(csvfile, sep='\t', index=False)
 
     #Write a yaml file
-    yamlfile = outdir+args.output+".yml"
+    yamlfile = outfile+".yml"
+    #yamlfile = outdir+args.output+".yml"
     writeFormattedYaml(yamlfile, final_df)
 
     #Write node specific files
     nodelist = final_df.Node.unique()
     for node in nodelist:
         node_df = final_df.loc[final_df['Node'] == node]
-        csvfilename = outdir+node+"_"+args.output+".tsv"
-        yamlfilename = outdir+node+"_"+args.output+".yml"
+        csvfilename = outfile+"_"+node+".tsv"
+        yamlfilename = outfile+"_"+node+".yml"
+        #csvfilename = outdir+node+"_"+args.output+".tsv"
+        #yamlfilename = outdir+node+"_"+args.output+".yml"
         writeFormattedYaml(yamlfilename, node_df)
         node_df.to_csv(csvfilename, sep='\t', index=False)
      
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--modelfile", required=True, help="MDF Model File")
-    parser.add_argument("-p", "--propfile", required=True, help = "MDF Property file") 
-    parser.add_argument("-d", "--handle", required=True, help= "MDF Model Handle name")                  
-    parser.add_argument("-o", "--output", required=True, help="Output name for csv and yml file")
+    #parser.add_argument("-m", "--modelfile", required=True, help="MDF Model File")
+    #parser.add_argument("-p", "--propfile", required=True, help = "MDF Property file") 
+    #parser.add_argument("-d", "--handle", required=True, help= "MDF Model Handle name")                  
+    #parser.add_argument("-o", "--output", required=True, help="Output name for csv and yml file")
+    parser.add_argument("-c", "--config", required=True, help="location and name of configuration file")
 
     args = parser.parse_args()
 
